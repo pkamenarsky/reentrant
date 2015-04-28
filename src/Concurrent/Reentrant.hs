e4bbd0ad83a070c3e4a64bdd33f3947f84513d32
Philip Kamenarsky <p.kamenarsky@gmail.com>
2015-04-28 15:47:12 +0200

    Experiments

--- src/Concurrent/Reentrant.hs
+++ src/Concurrent/Reentrant.hs
@@ -1,2 +1,12 @@
+{-# LANGUAGE ExistentialQuantification, DeriveFunctor, FlexibleContexts, ScopedTypeVariables, TemplateHaskell #-}
+
 module Concurrent.Reentrant where
 
+import Control.Monad.Free
+import Control.Monad.Free.TH
+
+data Reentrant a b = IO (a -> IO b)
+                   | Done
+  deriving Functor
+
+type ReentrantF a = Free (Reentrant a)
